a47f73a4d4f074c6ed2a432596eae794733a1332
Masoud-z <masoud.zeinalabedini@gmail.com>
2024-12-18 17:36:07 +0330

    Keep unit preference in local storage

--- src/core/store/useStore.ts
+++ src/core/store/useStore.ts
@@ -1,13 +1,22 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { Units } from "@/core/enums/units";
 
 export interface StoreState {
   unit: Units;
 }
 
-const useStore = create<StoreState>((set) => ({
-  unit: Units.Metric,
-  setUnit: (unit: Units) => set({ unit }),
-}));
+const useStore = create<StoreState, [["zustand/persist", { posts: Units }]]>(
+  persist(
+    (set, get) => ({
+      unit: Units.Metric,
+      setUnit: (unit: Units) => set({ unit }),
+    }),
+    {
+      name: "unit",
+      partialize: (state) => ({ posts: state.unit }), // Persist only the unit}
+    }
+  )
+);
 
 export default useStore;
